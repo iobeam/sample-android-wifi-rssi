6d2dad36ccb4d15979d5c968db5b09aa381b22bf
Rob Kiefer <rob.kiefer@gmail.com>
2015-04-09 18:07:56 -0400

    Removes unnecessary functions in Activity

--- app/src/main/java/com/iobeam/samples/android/wifirssi/IobeamActivity.java
+++ app/src/main/java/com/iobeam/samples/android/wifirssi/IobeamActivity.java
@@ -9,8 +9,6 @@ import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.iobeam.api.ApiException;
@@ -153,25 +151,6 @@ public class IobeamActivity extends ActionBarActivity implements Handler.Callbac
         mHandler.removeMessages(MSG_SEND_SUCCESS);
         mHandler.removeMessages(MSG_SEND_FAILURE);
         mHandler = null;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
